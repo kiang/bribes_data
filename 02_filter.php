3fdf12e01d560313ee73d9b8e181bb29814ff45e
kiang <kiange@gmail.com>
2019-12-27 23:58:58 +0800

    copy filtered cases to be pushed into repo

--- 02_filter.php
+++ 02_filter.php
@@ -1,4 +1,5 @@
 <?php
+$filterPath = __DIR__ . '/filter';
 $fh = fopen(__DIR__ . '/targets.csv', 'w');
 fputcsv($fh, array('file', 'case', 'title'));
 foreach(glob(__DIR__ . '/extract/*/*') AS $host) {
@@ -8,10 +9,18 @@ foreach(glob(__DIR__ . '/extract/*/*') AS $host) {
             $caseMatch = true;
         }
         if($caseMatch || mb_substr($host, -2, 2, 'utf-8') === '刑事') {
+            
             $pos = strpos($jsonFile, '/extract/') + 9;
             $json = json_decode(file_get_contents($jsonFile));
             if($caseMatch || false !== strpos($json->JTITLE, '選')) {
                 fputcsv($fh, array(substr($jsonFile, $pos), $json->JCASE, $json->JTITLE));
+
+                $targetFile = str_replace('/extract/', '/filter/', $jsonFile);
+                $p = pathinfo($targetFile);
+                if(!file_exists($p['dirname'])) {
+                    mkdir($p['dirname'], 0777, true);
+                }
+                copy($jsonFile, $targetFile);
             }
         }
     }
