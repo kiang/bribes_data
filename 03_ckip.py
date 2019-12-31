5a47bb686b2cc25d2b6e73fb4477dca8077457bf
kiang <kiange@gmail.com>
2019-12-31 13:03:58 +0800

    skip existed target

--- 03_ckip.py
+++ 03_ckip.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import csv, json, os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 from ckiptagger import WS, POS, NER
-
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
@@ -16,6 +17,8 @@ with open('targets.csv') as csvFile:
     for row in rows:
         with open('filter/' + row[0]) as jsonFile:
             targetFile = 'meta/' + row[0]
+            if os.path.exists(targetFile):
+                continue
             dirname = os.path.dirname(targetFile)
             if False == os.path.exists(dirname):
                 os.makedirs(dirname, 0o777)
